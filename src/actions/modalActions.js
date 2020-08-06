5ba8e46f1d44ab94401d29837c4be536eda35d59
douglasryu <douglasryu@hotmail.com>
2020-08-06 18:46:26 -0400

    Update website

--- src/actions/modalActions.js
+++
@@ -1,5 +0,0 @@
-export const OPEN_MODAL = "modal/OPEN_MODAL";
-export const CLOSE_MODAL = "modal/CLOSE_MODAL";
-
-export const openModal = () => ({ type: OPEN_MODAL, modal: "open" });
-export const closeModal = () => ({ type: CLOSE_MODAL });
--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import modalReducer from "../reducers/modalReducer";
-
-const rootReducer = combineReducers({
-    modal: modalReducer,
-});
-
-export default rootReducer;
